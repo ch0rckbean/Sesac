a5ab79802de337687e0bc847051f5b3bf6a0439a
chr0ckbean <2leebins@naver.com>
2023-08-11 16:12:15 +0900

    ejs에서 또 다른 ejs 불러오기, 정적 파일 열기, css 적용 등

--- 08_express/app.js
+++ 08_express/app.js
@@ -4,10 +4,20 @@ const PORT = 8080;
 
 app.set("view engine", "ejs"); //express 템플릿 엔진 종류(ejs) 등록
 app.set("views", "./views"); //템플릿 엔진 파일을 저장할 위치 등록
+//== app.use("/views", express.static(__dirname + "/views"));
+
+/* static 미들웨어 등록*/
+app.use("/public", express.static(__dirname + "/static"));
+//사용자에게 보여지는 url, 폴더명
+// console.log(__dirname); //c:\Users\user\Desktop\Sesac-Ydp-5\08_express
+// console.log(__dirname + "/static"); //c:\Users\user\Desktop\Sesac-Ydp-5\08_express/static
+// img 보려면
+// http://localhost:8080/public/img/zq.jpg
 
 //(임시) DB에서 가져온 회원 정보 (id,pw)
 const idFromDB = "Grape";
 const pwFromDB = "1234shine";
+const snack = "감자톡톡";
 
 /* app.get(경로, 해당 경로로 들어왔을 때 실행할 함수)*/
 // '/' : root경로. 서버주소, 포트번호(localhost:8080)
@@ -18,6 +28,7 @@ app.get("/", function (req, res) {
   res.render("index", {
     userId: idFromDB,
     userPw: pwFromDB,
+    snack: snack,
     btns: ["샤인", "머스캣", "마싯져"],
     jelly: {
       name: "Haribo",

38554f00953db5f0a75f7ed90de9043820dcc95b
ne0n9uy <2leebins@naver.com>
2023-08-31 16:18:21 +0900

    controller > Cuser.js sequelize

--- mvc_mysql_practice/controller/Cuser.js
+++ mvc_mysql_practice/controller/Cuser.js
@@ -4,40 +4,65 @@ exports.signup = (req, res) => {
   res.render("signup");
 };
 
-exports.postSignup = (req, res) => {
-  User.postSignup(req.body, () => {
-    res.end();
+exports.postSignup = async (req, res) => {
+  // User.postSignup(req.body, () => {
+  //   res.end();
+  // });
+  const { userid, pw, nickname } = req.body;
+  const result = await User.create({
+    userid,
+    pw,
+    nickname,
   });
+  console.log("CREATE result", result);
+  res.send(result);
 };
 
 exports.signin = (req, res) => {
   res.render("signin");
 };
 
-exports.postSignin = (req, res) => {
-  User.postSignin(req.body, (result) => {
-    if (result.length > 0) {
-      res.send(true);
-    } else {
-      res.send(false);
-    }
-  });
+exports.postSignin = async (req, res) => {
+  // User.postSignin(req.body, (result) => {
+  //   if (result.length > 0) {
+  //     res.send(true);
+  //   } else {
+  //     res.send(false);
+  //   }
+  // });
+  const result = await User.create({ result });
+  console.log("postSignup", result);
+  res.send(result);
 };
 
-exports.postProfile = (req, res) => {
-  User.postProfile(req.body.userid, (result) => {
-    res.render("profile", { data: result[0] });
+exports.postProfile = async (req, res) => {
+  // User.postProfile(req.body.userid, (result) => {
+  //   res.render("profile", { data: result[0] });
+  // });
+  const { id } = req.params;
+  const result = await User.findOne({
+    where: { id: id },
   });
+  res.render("profile", { data: result[0] });
 };
 
-exports.editProfile = (req, res) => {
-  User.editProfile(req.body, () => {
-    res.send(true);
-  });
+exports.editProfile = async (req, res) => {
+  // User.editProfile(req.body, () => {
+  //   res.send(true);
+  // });
+  const result=await User.update(
+    {nickname:req.body.nickname, pw:req.body.pw},
+    {where: {id:req.body.userid}}, 
+  );
+  res.send({true})
 };
 
-exports.deleteProfile = (req, res) => {
-  User.deleteProfile(req.body.id, () => {
-    res.send(true);
+exports.deleteProfile = async (req, res) => {
+  // User.deleteProfile(req.body.id, () => {
+  //   res.send(true);
+  // });
+  const result=await User.destroy({
+    where:{id:req.body.id}
   });
+  res.send(true)
 };

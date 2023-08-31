9c0107bb7c24574d165c08e1f0cdad339ce498d4
ne0n9uy <2leebins@naver.com>
2023-08-31 13:15:40 +0900

    solve 'ERR_HTTP_HEADERS_SENT' in Signup module due to FOR state

--- 16-2_mvc-mysql/model/User.js
+++ 16-2_mvc-mysql/model/User.js
@@ -12,7 +12,7 @@ const conn = mysql.createConnection({
 //** 회원가입
 exports.doSignup = (data, cb) => {
   console.log("data | doSignup | User.js", data);
-  const { id, pw, nickname } = data;
+  const { id, pw, nickname } = data; //Ctrlr에서 받아온 req.body
   conn.query(
     `INSERT INTO user(userid, name, pw) VALUES ("${id}","${pw}","${nickname}")`,
     (err, row) => {
@@ -21,13 +21,20 @@ exports.doSignup = (data, cb) => {
       }
       // console.log("data | doSignup | User.js", data);
       console.log("row | doSignup | User.js", row);
-      for (let i = 0; i < Object.keys(data).length; i++) {
-        if (Object.values(data)[i].length > 0) {
-          // console.log("ss", Object.values(data).length);
-          cb(row);
-        } else {
-          cb("Fail");
-        }
+      // for (let i = 0; i < Object.keys(data).length; i++) {
+      //   if (Object.values(data)[i].length > 0) {
+      //     // console.log("ss", Object.values(data).length);
+      //     cb(row); //ctrlr로 row를 보냄
+      //     //for문 내에 있어서 계속 ERR_HTTP_HEADERS_SENT 에러가 났었음 ㅠㅠ
+      //   }
+      if (
+        Object.values(data)[0].length &
+        Object.values(data)[1].length &
+        Object.values(data)[2].length
+      ) {
+        cb(row);
+      } else {
+        cb("Fail");
       }
     }
   );
@@ -51,3 +58,14 @@ exports.doSignin = (data, cb) => {
     }
   );
 };
+
+//** 프로필/
+// exports.editPro=(data,cb)=>{
+//   console.log(data);
+//   conn.query(`ALTER TABLE user SET username="${data.nickname},pw="${data.pw}" `),
+//   (err,row)=>{
+//     if(err){throw err}
+
+//   }
+
+// }

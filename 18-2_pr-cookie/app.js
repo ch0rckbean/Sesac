e6be859e1ca1a5ad7cdcf93f2a561cd19ae5d970
ne0n9uy <2leebins@naver.com>
2023-09-03 02:04:58 +0900

    cookie practice doing

--- 18-2_pr-cookie/app.js
+++ 18-2_pr-cookie/app.js
@@ -9,39 +9,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // TODO: cookie parser 미들웨어 등록
-COOKIE_SECRET_KEY = "secretKey";
-app.use(cookieParser(COOKIE_SECRET_KEY));
+app.use(cookieParser());
+// COOKIE_SECRET_KEY = "secretKey";
+// app.use(cookieParser(COOKIE_SECRET_KEY));
 
 // 하루 뒤의 날짜 구하기
 let tomorrow = new Date(); //오늘이 구해짐
 tomorrow.setDate(tomorrow.getDate() + 1); //하루 뒤로 날짜 설정
-console.log(tomorrow);
+console.log("tmr", tomorrow);
+console.log("tmr2", tomorrow.toGMTString());
 
 const popupConf = {
   httpOnly: true,
-  Expires: tomorrow.toGMTString,
-  signed: true,
+  Expires: tomorrow.toGMTString(),
+  // maxage: 24 * 60 * 60 * 1000, //1day
+  // signed: true,
 };
-console.log(tomorrow.toGMTString);
 app.get("/", (req, res) => {
   // *다음과 같이 기능 구현하였는데, 굳이 이렇게 하지 않아도 됩니다.
 
   // 모달 체크박스 체크시 -> GET / ; undefined
   // 모달 체크박스 미체크시 -> GET / ; hide
-  // console.log("req.cookies.popup >> ", req.cookies.popup);
-  console.log("req.signedCookies.popup", req.signedCookies.popup); //hide
+  console.log("req.cookies.popup >> ", req.cookies.popup);
+  // console.log("req.signedCookies.popup", req.signedCookies.popup); //hide
 
   // TODO: index.ejs render할 때 두 번째 인자로 popup key 로 요청의 쿠키값 보내기
-  res.render("cookie", { popup: req.signedCookies.popup });
+  res.render("cookie", { popup: req.cookies.popup }); //hide
+  // res.render("cookie", { popup: req.signedCookies.popup });
 });
 
 app.post("/setcookie", (req, res) => {
   // TODO: 쿠키 생성
   // 쿠키 이름: 'popup', 쿠키 값: 'hide'
-  console.log("res", res);
+  // console.log("res", res);
 
   res.cookie("popup", "hide", popupConf);
-  res.send({ data: res.cookie });
+  res.send("쿠키 설정 성공!");
 });
 
 app.listen(PORT, () => {

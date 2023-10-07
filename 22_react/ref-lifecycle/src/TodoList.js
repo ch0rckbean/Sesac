e2a212c400294e6c05e69687be4117acf466e7a8
ne0n9uy <2leebins@naver.com>
2023-10-08 00:51:04 +0900

    pr: 삭제 구현 중

--- 22_react/ref-lifecycle/src/TodoList.js
+++ 22_react/ref-lifecycle/src/TodoList.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 export default function TodoList() {
   const [todo, setTodo] = useState([]); //꼭 배열로 만들어야 map 사용 가능
   const [input, setInput] = useState(''); //변수로 input을 가져오기
+  const [checkedList, setCheckedList] = useState([]);
 
   const register = () => {
     if (todo.length >= 10) {
@@ -16,7 +17,20 @@ export default function TodoList() {
     }
   };
 
-  const deleteTodo = () => {};
+  const handleCheck = (isChecked, checkedId) => {
+    if (isChecked) {
+      setCheckedList([...checkedList, checkedId]);
+    } else {
+      setCheckedList(checkedList.filter((todoId) => todoId !== checkedId));
+    }
+    console.log('cl', checkedList);
+  };
+  const deleteTodo = () => {
+    //삭제될 일이 체크됐으면
+    const newTodo2 = todo.filter((todo) => todo.id !== todo.isChecked);
+    console.log('checkedList', checkedList);
+    setTodo([...todo, newTodo2]);
+  };
 
   return (
     <div>
@@ -36,8 +50,11 @@ export default function TodoList() {
               <li key={todo.id}>
                 <input
                   type='checkbox'
-                  id='todoCheckbox'
-                  onChange={(e) => e.target.checked}
+                  onChange={(e) =>
+                    handleCheck(e.target.checked, e.target.value)
+                  }
+                  value={todo.val}
+                  // checked={checkedList.includes(todo.value) ? true : false}
                 />
                 {todo.val}
               </li>

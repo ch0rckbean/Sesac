f3a24780f777830c52b5d482eefa933fc0208283
ne0n9uy <2leebins@naver.com>
2023-10-08 11:09:34 +0900

    pr: 체크박스 핸들러 & 삭제 기능 구현 완료

--- 22_react/ref-lifecycle/src/TodoList.js
+++ 22_react/ref-lifecycle/src/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function TodoList() {
   const [todo, setTodo] = useState([]); //꼭 배열로 만들어야 map 사용 가능
@@ -18,18 +18,32 @@ export default function TodoList() {
   };
 
   const handleCheck = (isChecked, checkedId) => {
+    checkedId = Number(checkedId); //왜 str으로 받아오지? 숫자로 변환해야 동작
     if (isChecked) {
+      console.log('체크 됨');
       setCheckedList([...checkedList, checkedId]);
     } else {
-      setCheckedList(checkedList.filter((todoId) => todoId !== checkedId));
+      console.log('체크 해제');
+      setCheckedList(
+        checkedList.filter((chekckListId) => chekckListId !== checkedId)
+        // chekckList 안에 있는 Id 값과 같은 checkedId는 다 뺌
+      );
     }
-    console.log('cl', checkedList);
   };
+
+  useEffect(() => {
+    //배열을 동기적으로 만듦
+    console.log('cl', checkedList);
+  }, [checkedList]);
+
   const deleteTodo = () => {
     //삭제될 일이 체크됐으면
-    const newTodo2 = todo.filter((todo) => todo.id !== todo.isChecked);
-    console.log('checkedList', checkedList);
-    setTodo([...todo, newTodo2]);
+    if (checkedList) {
+      const newTodo2 = todo.filter((todo) => !checkedList.includes(todo.id));
+      console.log('newTodo2', newTodo2);
+      setTodo(newTodo2);
+      setCheckedList([]); // 체크 배열 초기화 필요
+    }
   };
 
   return (
@@ -50,10 +64,9 @@ export default function TodoList() {
               <li key={todo.id}>
                 <input
                   type='checkbox'
-                  onChange={(e) =>
-                    handleCheck(e.target.checked, e.target.value)
-                  }
+                  onChange={(e) => handleCheck(e.target.checked, e.target.id)}
                   value={todo.val}
+                  id={todo.id}
                   // checked={checkedList.includes(todo.value) ? true : false}
                 />
                 {todo.val}

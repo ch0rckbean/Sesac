e53cdb8c5cfdbf39d20dcd27fca83188e832a45e
chr0ckbean <2leebins@naver.com>
2023-10-11 15:22:52 +0900

    study: SASS

--- 22_react/styles/src/App.js
+++ 22_react/styles/src/App.js
@@ -1,5 +1,6 @@
 import './App.js';
 import CssModuleComponents from './CssModuleComponents.js';
+import SassComponent from './SassComponent.js';
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       <h1>React Style</h1>
       <h2>CSS Module</h2>
       <CssModuleComponents />
+      <h2>SASS</h2>
+      <SassComponent />
     </div>
   );
 }

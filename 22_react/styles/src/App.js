6ceb5e15e404e47fc622bfa3064cce38e1735698
chr0ckbean <2leebins@naver.com>
2023-10-11 15:47:25 +0900

    study: styled-components

--- 22_react/styles/src/App.js
+++ 22_react/styles/src/App.js
@@ -1,6 +1,7 @@
 import './App.js';
 import CssModuleComponents from './CssModuleComponents.js';
 import SassComponent from './SassComponent.js';
+import StyledComponent from './styles/StyledComponent.js';
 
 function App() {
   return (
@@ -10,6 +11,14 @@ function App() {
       <CssModuleComponents />
       <h2>SASS</h2>
       <SassComponent />
+      <h2>StyledComponent</h2>
+      <StyledComponent />
+      <hr></hr>
+      <br></br>
+      <h1>실습</h1>
+      <br></br>
+
+      <br></br>
     </div>
   );
 }

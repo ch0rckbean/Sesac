f989d74e97dea4bac444f0fb931aa1e8bec9378a
ne0n9uy <2leebins@naver.com>
2023-10-17 23:00:47 +0900

    study: 복습 및 email, password form validation 추가

--- 22_react/hooks/src/Components/react-hook-form/Form.js
+++ 22_react/hooks/src/Components/react-hook-form/Form.js
@@ -9,7 +9,7 @@ export default function Form() {
     watch, //특정 form 필드의 값을 실시간으로 사용
   } = useForm();
 
-  // handleSubmit(funcA[, funcB]) : 두 개의 함수를 받음
+  // handleSubmit(funcA , [funcB]) : 두 개의 함수를 받음
   // - funcA: 필수, 유효할 때 실행
   // - funcB: 선택, 유효하지 않을 때 실행
   const onValid = (data) => {
@@ -22,24 +22,25 @@ export default function Form() {
 
   //   console.log('errors', errors);
   //   console.log('watch', watch('username'));
-
   return (
     <div>
       <h1>react-hook-form</h1>
       <form onSubmit={handleSubmit(onValid, onInValid)}>
         <input
-          type="text"
-          placeholder="username"
+          type='text'
+          placeholder='username'
           {...register('username', {
             required: '이름을 입력해주세요',
             minLength: { message: '최소 2글자 이상 작성해주세요.', value: 2 },
           })}
         />
         {errors.username?.message}
+        {/* errors에 username 있으면 message 출력 */}
         <br></br>
+
         <input
-          type="email"
-          placeholder="email"
+          type='email'
+          placeholder='email'
           {...register('email', {
             required: '이메일을 입력해주세요',
             validate: {
@@ -50,8 +51,16 @@ export default function Form() {
         />
         {errors.email?.message}
         <br></br>
-        <input type="text" placeholder="password" {...register('password')} />
-        <button type="submit">Submit</button>
+        <input
+          type='password'
+          placeholder='password'
+          {...register('password', {
+            minLength: { message: '최소 2글자 이상 작성해주세요.', value: 2 },
+          })}
+        />
+        {errors.password?.message}
+        <br></br>
+        <button type='submit'>Submit</button>
       </form>
     </div>
   );
--- 22_react/hooks/src/Components/react-hook-form/PrForm.js
+++ 22_react/hooks/src/Components/react-hook-form/PrForm.js
@@ -16,24 +16,24 @@ export default function PrForm() {
     console.log('onInValid', err);
   };
 
+  console.log(errors);
   console.log(isSubmitted);
   return (
     <div>
       <h1>react-hook-form 실습</h1>
       <form onSubmit={handleSubmit(onValid, onInValid)}>
         <input
-          type="text"
-          placeholder="name"
+          type='text'
+          placeholder='name'
           {...register('name', {
             required: '이름은 필수 항목입니다.',
           })}
         />{' '}
-        <br></br>
-        {errors?.name?.message}
+        {errors.name?.message}
         <br></br>
         <input
-          type="text"
-          placeholder="age"
+          type='text'
+          placeholder='age'
           {...register('age', {
             validate: {
               useNum: (v) =>
@@ -41,9 +41,30 @@ export default function PrForm() {
             },
           })}
         />
+        {errors.age?.message}
         <br></br>
-        {errors?.age?.message}
-        <button type="submit">SUBMIT</button>
+        <input
+          type='text'
+          placeholder='nickname'
+          {...register('nickname', {
+            minLength: { message: '최소 2글자 이상 작성해주세요', value: 2 },
+          })}
+        />
+        {errors.nickname?.message}
+        <br></br>
+        <input
+          type='email'
+          placeholder='email'
+          {...register('email', {
+            validate: {
+              useCom: (v) =>
+                v.includes('com') || '올바른 이메일 형식이 아닙니다.',
+            },
+          })}
+        />
+        {errors.email?.message}
+        <br></br>
+        <button type='submit'>SUBMIT</button>
       </form>
     </div>
   );
